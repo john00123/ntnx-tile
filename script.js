85d8dae9439c5e46f02311fdba80c012bbde9d24
John <john.torres.hurtado@gmail.com>
2017-02-05 21:33:35 -0800

    Changes

---
+++ script.js
@@ -0,0 +1,15 @@
+right.onclick = myFunction;
+function myFunction() {
+document.getElementsByClassName("title1")[0].style.display = "none";
+document.getElementsByClassName("title2")[0].style.display = "block";
+document.getElementsByClassName("alpha")[0].style.display = "none";
+document.getElementsByClassName("beta")[0].style.display = "block";
+}
+
+left.onclick = myFunction1;
+function myFunction1() {
+document.getElementsByClassName("title2")[0].style.display = "none";
+document.getElementsByClassName("title1")[0].style.display = "block";
+document.getElementsByClassName("beta")[0].style.display = "none";
+document.getElementsByClassName("alpha")[0].style.display = "block";
+}
